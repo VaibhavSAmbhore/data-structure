c710499fee1c36822049f0340c471aa6d26422d0
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-08-31 14:47:40 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,15 @@
+class GFG
+{
+	public static void main(String arg[])
+	{
+	  //creating an integer array
+	  // named arr of size 10
+	  int [] arr = new int[10];
+	  //accresssing element at 0 index
+	  //and setting its value to 5 
+	  arr[0]=5;
+	  //access and print value at 0
+	  //index we get the output at 5
+	  System.out.println(arr[0]);
+	}
+}
